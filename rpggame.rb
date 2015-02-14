876dafe1adf69028aa415d62b51c7f07aabdf741
ClassyCritic <cantucade@gmail.com>
2015-02-14 12:18:15 -0500

    Lots of options added

--- rpggame.rb
+++ rpggame.rb
@@ -40,6 +40,31 @@ options1 = ["You fall off a cliff and die.", "You find a house.", "You come acro
 				useropt110 = gets.chomp.to_i
 				options110 = ["You attempt to run from the man.", "You greet the mysterious man.", "You fight the newly encountered man."]
 				puts options110[useropt110]
+					if useropt110 == 0
+						puts "You quicly run back towards the door!"
+						puts "The woman slams the door shut."
+						puts "The woman says, 'you're not leaving now precious'. This is the end of the line."
+						puts "Game over! Please play again!"
+					elsif useropt110 == 1
+						puts "You introduce yourself to the man, he's very friendly!"
+						puts "Choose a number 0-2 for your next decision."
+						useropt1101 = gets.chomp.to_i
+						options1101 = ["You decide to stay with the man and woman forever. You end your journey peacefully.", "You decide to leave the house, this isn't where you're meant to be.", "You decide to stay temporarily."]
+						puts options1101[useropt1101]
+							if useropt1101 == 0 
+							puts "Game over! Please play again."
+							elsif useropt1101 == 1
+							puts "As soon as you leave the house you're attacked by a group of bandits. Your last memory is seeing the man and woman being taken captive. This is the end of the line."
+							puts "Game over! Please play again."
+							else 
+							puts "As time goes on, you decide you want to stay. You spend the rest of your days with the family and die of old age."
+							puts "Game over! Please play again."
+							end
+					else 
+						puts "The man doesn't hesitate to kill you. This is the end of the line."
+						puts "Game over! Please play again."
+					end
+						
 			elsif useropt11 == 1
 				puts "You walk around the house and you seen an old lady, tending to her garden."
 				puts "Choose a number 0-2 for your next decision."
@@ -64,12 +89,39 @@ options1 = ["You fall off a cliff and die.", "You find a house.", "You come acro
 				useropt120 = gets.chomp.to_i
 				options120 = ["You meet a man and a woman. They give you food and a place to stay.", "You attack the house, hoping to steal all provisions.", "You walk past the house, and back into the forest."]
 				puts options120[useropt120]
+					if useropt120 == 0
+						puts "The man and woman are very friendly, but it's time to make a decision."
+						puts "Choose a number 0-2 for your next decision."
+						useropt1200 = gets.chomp.to_i
+						options1200 = ["You stay at the house for the rest of your life, farming and cooking.", "You decide you must leave and continue your journey.", "You decide you'll stay a while longer."]
+						puts options1200[useropt1200]
+					elsif useropt120 == 1
+						puts "You barge in through the door, sword at the ready..."
+						puts "The man overcomes you in single combat. This is the end of the line."
+						puts "Thanks for playing. Please play again."
+					else 
+						puts "You near the forest and can't help but notice the dangers it holds. But there's no turning back now."
+						puts "Choose a number 0-2 for your next decision."
+						useropt1202 = gets.chomp.to_i
+						options1202 = ["You charge into the forest, but it's many beasts soon overcome you. This is the end of the line", "As you charge, your ankle gives out from beneath you. This is the end of the line.", "You climb a tree and attempt to hide from the many dangers."]
+						puts options1202[useropt1202]
+					end
 			elsif useropt12 == 1
 				puts "You draw your sword and enter the forest...."
 				puts "Choose a number 0-2 for your next decision."
 				useropt121 = gets.chomp.to_i
-				options121 = ["You fight fiercely, but in the end you're overcome by the beasts of the forests.", "You're fighting when suddenly a man appears. He clears the area and takes you back to a dark, cold cave.", "You enter the forest and surrender at once. It's the end of the line."]
+				options121 = ["You fight fiercely, but in the end you're overcome by the beasts of the forests. This is the end of the line.", "You're fighting when suddenly a man appears. He clears the area and takes you back to a dark, cold cave.", "You enter the forest and surrender at once. It's the end of the line."]
 				puts options121[useropt121]
+					if useropt121 == 1
+						puts "Back in the cave the man is asleep..."
+						puts "Choose a number 0-2 for your next decision."
+						useropt1211 = gets.chomp.to_i
+						options1211 = ["You attack the man, hoping to escape.", "You attempt to sneak from the cave.", "You wake the man, and ask for more details."]
+						puts options1211[useropt1211]
+					else 
+					puts "Game over. Please play again."
+					end
+				
 			else
 				puts "You tip-toe into the dark, intimidating forest..."
 				puts "Choose a number 0-2 for your next decision."
